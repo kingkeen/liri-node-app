f9abc1c829e2d6154220f8fcaa7888bcc1d1e0eb
Joaquin Ignacio <Keen@Revolt.local>
2017-07-09 00:57:41 -0700

    file load

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log("Keys are loaded");
+
+exports.twitterKeys = {
+	consumer_key: 'OLBLgWYecL2iH54BTf0z08Qpq',
+	consumer_secret: '8HFrmXStcVM9vmbCHEVv3oEO0bwuV0A2lsJk9pBE6V0O3qc5pZ',
+	access_token_key: '731551558006013952-1sykUPKHNYVGhLi9b9z8MBGUtGB5Khx',
+	access_token_secret: 'ImB2TwYkhPHnivjEvloR0hQbhqduwo52INvpE2mIL1q2U',
+}
+
+exports.spotifyKeys = {
+	client_id: '450637ff1bdc42d9aca3211c8791bc14',
+	client_secret: 'f15f520581fa44ff8679371ce9285d3f',
+}
---
+++ liri.js
@@ -0,0 +1,2 @@
+
+//Code needed to grab information from Keys.js here.
